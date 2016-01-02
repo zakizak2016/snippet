0adaf977e3f9ff187c7308e351c5eaecb3fc604f
zaki <zakizak2016@yahoo.com>
2016-01-02 13:54:58 +0000

    Update Gruntfile.startup.js

--- Gruntfile.startup.js
+++ Gruntfile.startup.js
@@ -5,17 +5,60 @@ module.exports = function(grunt) {
             options: {
                 separator: ';',
             },
-            dist: {
-                src: ['node_modules/bootstrap/dist/js/bootstrap.min.js', 'src/outro.js'],
+            js: {
+                src: [
+                // "common-files/js/jquery-1.10.2.min.js",
+                // "flat-ui/js/bootstrap.min.js",
+                "common-files/js/jquery.scrollTo-1.4.3.1-min.js",
+                "common-files/js/modernizr.custom.js",
+                "common-files/js/page-transitions.js",
+                "common-files/js/easing.min.js",
+                "common-files/js/jquery.svg.js",
+                "common-files/js/jquery.svganim.js",
+                "common-files/js/jquery.parallax.min.js",
+                "common-files/js/startup-kit.js"
+                ],
                 dest: 'built.js',
             },
+            css: {
+                src: [
+                // "flat-ui/bootstrap/css/bootstrap.css",
+                "flat-ui/css/flat-ui.css",
+                "common-files/css/icon-font.css",
+                "common-files/css/animations.css",
+                "ui-kit/ui-kit-header/css/style.css",
+                "ui-kit/ui-kit-content/css/style.css",
+                "ui-kit/ui-kit-blog/css/style.css",
+                "ui-kit/ui-kit-contacts/css/style.css",
+                "ui-kit/ui-kit-crew/css/style.css",
+                "ui-kit/ui-kit-price/css/style.css",
+                "ui-kit/ui-kit-projects/css/style.css",
+                "ui-kit/ui-kit-footer/css/style.css"
+                ],
+                dest: 'built.css',
+            },
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'built.min.js': ['built.js']
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'built.min.css': ['built.css']
+                }
+            }
         },
         watch: {
             scripts: {
-                // files: ['**/*.js'],
                 files: [
-                '*.less',
-                '*.php',
                 'startup/**/**/**/*.less'
                 ],
                 tasks: ['less:development'],
@@ -40,10 +83,14 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['watch']);
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+grunt.registerTask('default', ['watch']);
 
     /* 
     Live reload
