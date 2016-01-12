8aa427517bcb44918bc743f37ae3aa9296edb2ff
zaki <zakizak2016@yahoo.com>
2016-01-12 16:36:10 +0000

    Update CI_NodeGeneratorSimpleVersion.js

--- CI_NodeGeneratorSimpleVersion.js
+++ CI_NodeGeneratorSimpleVersion.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var args = process.argv.slice(2);
 
-
+var mode = "frontend";
 
 var bundleName = args[0];
 var bundleLowercase = bundleName.toLowerCase();
 var bundleFirstUpper = bundleName.charAt(0).toUpperCase() + bundleName.toLowerCase().slice(1);
-var controllers = 'application/controllers/frontend/' + bundleFirstUpper + '.php' ;
-var views = 'application/views/frontend/' + bundleLowercase + '.php';
+var controllers = 'application/controllers/'+mode+'/' + bundleFirstUpper + '.php' ;
+var viewFolder = 'application/views/'+mode+'/' + bundleLowercase ;
+var views = 'application/views/'+mode+'/' + bundleLowercase +'/'+ bundleLowercase + '_view.php';
+
 
 
 /*
@@ -21,5 +23,12 @@ fs.writeFile(views, '', function (err) {
 });
 */
 
+/*
 fs.unlink(controllers);
 fs.unlink(views);
+*/
+
+/* Use sync*/
+fs.writeFileSync(controllers);
+fs.mkdirSync(viewFolder);
+fs.writeFileSync(views);
